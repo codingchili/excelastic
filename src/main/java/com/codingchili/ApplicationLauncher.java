e47660a356e0ff21c01307873bb0072b9debc415
Robin Duda <robin.duda@idainfront.se>
2018-04-28 15:38:37 +0200

    Moved the commandline importer into its own controller.

--- src/main/java/com/codingchili/ApplicationLauncher.java
+++ src/main/java/com/codingchili/ApplicationLauncher.java
@@ -1,25 +1,18 @@
 package com.codingchili;
 
+import com.codingchili.Controller.CommandLine;
+import com.codingchili.Controller.Website;
+import com.codingchili.Model.*;
+import com.codingchili.logging.ApplicationLogger;
+import io.vertx.core.*;
+import io.vertx.core.eventbus.MessageConsumer;
+
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import com.codingchili.Controller.Website;
-import com.codingchili.Model.Configuration;
-import com.codingchili.Model.ElasticWriter;
-import com.codingchili.Model.FileParser;
-import com.codingchili.Model.ParserException;
-
-import io.vertx.core.*;
-import io.vertx.core.eventbus.DeliveryOptions;
-import io.vertx.core.eventbus.MessageConsumer;
-
-import static com.codingchili.Model.ElasticWriter.ES_STATUS;
-import static com.codingchili.Model.ElasticWriter.INDEXING_TIMEOUT;
+import static com.codingchili.Model.ElasticWriter.*;
 
 /**
  * @author Robin Duda
@@ -27,110 +20,68 @@ import static com.codingchili.Model.ElasticWriter.INDEXING_TIMEOUT;
  * Launcher class to bootstrap the application.
  */
 public class ApplicationLauncher {
-    private static final Logger logger = Logger.getLogger(ApplicationLauncher.class.getName());
-    public static final String PARAM_CLEAR = "--clear";
-    public static String VERSION = "1.2.6";
-    private Vertx vertx = Vertx.vertx();
-    private String[] args;
+    private final ApplicationLogger logger = new ApplicationLogger(getClass());
+    public static String VERSION = "1.2.7";
+    private Vertx vertx;
 
     public static void main(String[] args) {
         new ApplicationLauncher(args);
     }
 
-    public ApplicationLauncher(String[] args) {
-        this.args = args;
+    private ApplicationLauncher(String[] args) {
+        VertxOptions options = new VertxOptions();
+        options.setMaxEventLoopExecuteTime(options.getMaxEventLoopExecuteTime() * 10)
+                .setBlockedThreadCheckInterval(8000);
+        vertx = Vertx.vertx();
 
-        logger.info(String.format("Starting excelastic %s..", VERSION));
-        logger.info("to import files without the web interface use: <filename> <index> <mapping>");
-        Future<Void> future = Future.future();
+        ImportEventCodec.registerOn(vertx);
 
-        future.setHandler(done -> {
+        logger.startupMessage();
+
+        start().setHandler(done -> {
             if (done.succeeded()) {
-                logger.info("Successfully started application");
+                logger.applicationStartup();
 
                 if (args.length > 1) {
-                    importFile(getFileName(), getIndexName());
+                    // import file from the command line.
+                    new CommandLine(vertx, args);
                 } else {
-                    MessageConsumer<?> consumer = vertx.eventBus().consumer(ES_STATUS);
-                    consumer.handler(message -> {
-                        logger.info(String.format("Attempting to open browser.. [ES connected=%s]", message.body().toString()));
-                        try {
-                            Desktop.getDesktop().browse(new URI(Configuration.getWebsiteURL()));
-                        } catch (IOException | URISyntaxException e) {
-                            logger.warning(e.getMessage());
-                        }
-                        consumer.pause();
-                    });
+                    // wait for the elasticsearch server to come online to show the import UI.
+                    waitForElasticServerAvailability();
                 }
             } else {
-                logger.log(Level.SEVERE, "Failed to start application", done.cause());
+                logger.applicationStartupFailure(done.cause());
                 vertx.close();
             }
         });
-        start(future);
-    }
-
-    private String getFileName() {
-        return args[0];
-    }
-
-    private String getIndexName() {
-        return args[1];
-    }
-
-    private Boolean clearExisting() {
-        return Arrays.stream(args).anyMatch(param -> param.equals(PARAM_CLEAR));
-    }
-
-    private String getMapping() {
-        return (args.length > 2) ? args[2] : "default";
     }
 
     /**
-     * Imports a file from the commandline.
-     * @param fileName the name of the file to import.
-     * @param indexName the name of the index to import the file into.
+     * Waits until there is a connection to the ElasticSearch server and then
+     * attempts to open the browser if one is available to the import website.
      */
-    private void importFile(String fileName, String indexName) {
-        logger.info(String.format("Loading file %s from filesystem..", fileName));
-        vertx.fileSystem().readFile(fileName, file -> {
-            if (file.succeeded()) {
-                logger.info("Parsing XLSX file to JSON..");
-                try {
-                    FileParser parser = new FileParser(file.result().getBytes(), 1, fileName);
-                    logger.info(String.format("File parsed, starting import to %s..", indexName));
-                    vertx.eventBus().send(Configuration.INDEXING_ELASTICSEARCH,
-                            parser.toImportable(indexName, getMapping(), clearExisting()),
-                            new DeliveryOptions().setSendTimeout(INDEXING_TIMEOUT),
-                            done -> {
-                        if (done.succeeded()) {
-                            logger.info("Import completed, shutting down.");
-                        } else {
-                            logger.log(Level.SEVERE, "Failed to import", done.cause());
-                        }
-                        System.exit(0); // vertx.close gives an error: "result already completed: success"
-                    });
-                } catch (ParserException e) {
-                    logger.log(Level.SEVERE, String.format("Failed to import file %s", fileName));
-                }
-            } else {
-                logger.log(Level.SEVERE, String.format("Failed to load file %s", fileName), file.cause());
+    private void waitForElasticServerAvailability() {
+        MessageConsumer<?> consumer = vertx.eventBus().consumer(ES_STATUS);
+        consumer.handler(message -> {
+            logger.openingBrowser(message.body().toString());
+            try {
+                Desktop.getDesktop().browse(new URI(Configuration.getWebsiteURL()));
+            } catch (IOException | URISyntaxException e) {
+                logger.onError(e);
             }
+            consumer.pause();
         });
     }
 
-    public void start(Future<Void> start) {
+    /**
+     * Deploys the elastic writer and the website.
+     * @return application deployment callback.
+     */
+    public CompositeFuture start() {
         Future<String> writer = Future.future();
         Future<String> website = Future.future();
         vertx.deployVerticle(new ElasticWriter(), writer.completer());
         vertx.deployVerticle(new Website(), website.completer());
-
-        CompositeFuture.all(writer, website).setHandler(done -> {
-            if (done.succeeded()) {
-                start.complete();
-            } else {
-                start.fail(done.cause());
-            }
-        });
+        return CompositeFuture.all(writer, website);
     }
 }
---
+++ src/main/java/com/codingchili/Controller/CommandLine.java
@@ -0,0 +1,74 @@
+package com.codingchili.Controller;
+
+import com.codingchili.Model.*;
+import com.codingchili.logging.ApplicationLogger;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.DeliveryOptions;
+
+import java.io.*;
+
+import static com.codingchili.Model.ElasticWriter.INDEXING_TIMEOUT;
+
+/**
+ * @author Robin Duda
+ * <p>
+ * Implements the commandline import functionality.
+ */
+public class CommandLine {
+    private ApplicationLogger logger = new ApplicationLogger(CommandLine.class);
+    private Vertx vertx;
+
+    public CommandLine(Vertx vertx, String[] args) {
+        this.vertx = vertx;
+        assertCommandLineValid(args);
+        importFile(ImportEvent.fromCommandLineArgs(args), args[0]);
+    }
+
+    /**
+     * Makes sure that the given commandline options contains all the required values.
+     *
+     * @param args the commandline arguments.
+     */
+    private void assertCommandLineValid(String[] args) {
+        if (args.length < 2) {
+            vertx.close(closed -> logger.onCommandLineMissingArguments(args));
+        }
+    }
+
+
+    /**
+     * Imports a file from the commandline.
+     *
+     * @param event    the import event - contains information like index and mapping to use.
+     * @param fileName the name of the file to be imported.
+     */
+    private void importFile(ImportEvent event, String fileName) {
+        logger.loadingFromFilesystem(fileName);
+        logger.parsingStarted();
+        try {
+            FileParser parser = new FileParser(new File(fileName), 1, fileName);
+            event.setParser(parser);
+            parser.assertFileParsable();
+
+            logger.importStarted(event.getIndex());
+            vertx.eventBus().send(Configuration.INDEXING_ELASTICSEARCH, event, getDeliveryOpts(),
+                    done -> {
+                        if (done.succeeded()) {
+                            logger.importCompleted();
+                        } else {
+                            logger.onImportFailed(done.cause());
+                        }
+                        System.exit(0); // vertx.close gives an error: "result already completed: success"
+                    });
+        } catch (ParserException e) {
+            logger.onParseFailed(fileName, e);
+        } catch (FileNotFoundException e) {
+            logger.onFileLoadFailed(fileName, e);
+        }
+    }
+
+    private DeliveryOptions getDeliveryOpts() {
+        return new DeliveryOptions().setSendTimeout(INDEXING_TIMEOUT);
+    }
+
+}
